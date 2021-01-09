d45ea3254ff0ea3f0b106434fed27ed21672e981
Brandon Semilla <semibran@gmail.com>
2021-01-09 14:02:14 -0800

    lay out basic page states

--- src/popup.js
+++ src/popup.js
@@ -14,4 +14,36 @@ const state = {
   spotify: {}
 }
 
-m.render(document.body, 'Hello world!')
+const popup = {
+  view () {
+    const view = pages[state.page]
+    if (view) {
+      return view()
+    }
+  }
+}
+
+const pages = {
+  initial () {
+    return m('main', [
+      m('h1', 'Initial state'),
+      m('button', 'Start')
+    ])
+  },
+  playing () {
+    return m('main', [
+      m('h1', 'Playing state'),
+      m('button', 'Pause'),
+      m('button', 'Stop')
+    ])
+  },
+  complete () {
+    return m('main', [
+      m('h1', 'Complete state'),
+      m('button', 'Snooze'),
+      m('button', 'Ok')
+    ])
+  }
+}
+
+m.mount(document.body, popup)
