4cc2dd6a946ebbf5d7a515ae14ad138ef8a7d4c9
Brandon Semilla <semibran@gmail.com>
2021-01-09 13:37:55 -0800

    add state object

--- src/popup.js
+++ src/popup.js
@@ -1,3 +1,17 @@
 import m from 'mithril'
 
+const state = {
+  page: 'initial',
+  time: 0,
+  paused: false,
+  noise: {
+    color: 'white'
+  },
+  beats: {
+    pitch: 200,
+    pattern: 'beta'
+  },
+  spotify: {}
+}
+
 m.render(document.body, 'Hello world!')
