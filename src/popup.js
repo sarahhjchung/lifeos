def9e9f529e532f2f4d4449f8f78786530c7a2ee
sarahhjchung <60327675+sarahhjchung@users.noreply.github.com>
2021-01-09 15:27:17 -0800

    changed initial state

--- src/popup.js
+++ src/popup.js
@@ -34,8 +34,9 @@ const togglePause = () => {
 const pages = {
   initial () {
     return m('main', [
-      m('h1', 'Initial state'),
-      m('button', { onclick: () => gotoPage('playing') }, 'Start')
+      m('h1', {class: "title"}, 'LifeOS'),
+      m('h3', {class: "message"}, 'How long would you like to work?'),
+      m('button', {class: "start button", onclick: () => gotoPage('playing') }, 'Start >')
     ])
   },
   playing () {
