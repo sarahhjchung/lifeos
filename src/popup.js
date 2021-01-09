dec9616e79717123b37fbb8762c29c9bee58a0ac
Brandon Semilla <semibran@gmail.com>
2021-01-09 14:06:13 -0800

    add some actions

--- src/popup.js
+++ src/popup.js
@@ -3,7 +3,7 @@ import m from 'mithril'
 const state = {
   page: 'initial',
   time: 0,
-  paused: false,
+  pause: false,
   noise: {
     color: 'white'
   },
@@ -23,25 +23,33 @@ const popup = {
   }
 }
 
+const gotoPage = (page) => {
+  state.page = page
+}
+
+const togglePause = () => {
+  state.pause = !state.pause
+}
+
 const pages = {
   initial () {
     return m('main', [
       m('h1', 'Initial state'),
-      m('button', 'Start')
+      m('button', { onclick: () => gotoPage('playing') }, 'Start')
     ])
   },
   playing () {
     return m('main', [
       m('h1', 'Playing state'),
-      m('button', 'Pause'),
-      m('button', 'Stop')
+      m('button', { onclick: togglePause }, 'Pause'),
+      m('button', { onclick: () => gotoPage('initial') }, 'Stop')
     ])
   },
   complete () {
     return m('main', [
       m('h1', 'Complete state'),
       m('button', 'Snooze'),
-      m('button', 'Ok')
+      m('button', { onclick: () => gotoPage('initial') }, 'Ok')
     ])
   }
 }
