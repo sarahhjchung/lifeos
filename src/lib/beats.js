276530658cf61f0426cc7b7f0ea504d4ec0c1faa
Ning Ding <n2dingn2ding@gmail.com>
2021-01-10 00:08:03 -0800

    clean up beats

--- src/lib/beats.js
+++ src/lib/beats.js
@@ -1,34 +1,32 @@
 // create web audio api context
 const context = new window.AudioContext()
 
+// left channel
 const splitter = context.createChannelSplitter(2)
 const merger = context.createChannelMerger(2)
-
-
 const left = context.createOscillator()
 left.type = 'square'
 left.frequency.setValueAtTime(440, context.currentTime)
-
 left.connect(splitter)
 splitter.connect(merger, 0, 1)
 
+// right channel
 const splitter2 = context.createChannelSplitter(2)
 const merger2 = context.createChannelMerger(2)
-
 const right = context.createOscillator();
 right.type = 'square';
 right.frequency.setValueAtTime(400, context.currentTime)
-
 right.connect(splitter2)
 right.connect(merger2, 0, 0)
 
-
 export function playBeats () {
-    merger.connect(context.destination)
-    left.start()
-    merger2.connect(context.destination)
-    right.start()
+  merger.connect(context.destination)
+  left.start()
+  merger2.connect(context.destination)
+  right.start()
 }
 
-
-
+export function stopBeats () {
+  left.stop()
+  right.stop()
+}
