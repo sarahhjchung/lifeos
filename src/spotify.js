bcdfe8f5d110a81ea547e008511bb7dcdbab32af
Brandon Semilla <semibran@gmail.com>
2021-01-09 17:43:12 -0800

    switch back to make

--- src/spotify.js
+++ src/spotify.js
@@ -1,18 +1,26 @@
 import clientId from './client.json'
 
+const chrome = window.chrome // eslint prefix
 const endpoint = 'https://accounts.spotify.com/authorize'
 const scopes = [
   'user-read-currently-playing',
   'user-read-playback-state'
 ]
 
-const href = endpoint +
-  '?client_id=' + clientId +
-  '&scope=' + scopes.join('%20') +
-  '&redirect_uri=http://localhost:8888/callback' +
-  '&response_type=token' +
-  '&show_dialog=true'
+function getURL (endpoint, clientId, scopes) {
+  const redirectURL = chrome.identity.getRedirectURL('callback')
+  return endpoint +
+    '?client_id=' + clientId +
+    '&scope=' + scopes.join('%20') +
+    '&redirect_uri=' + redirectURL +
+    '&response_type=token' +
+    '&show_dialog=true'
+}
 
 export default function openSpotify () {
-  window.chrome.tabs.create({ url: href })
+  const req = getURL(endpoint, clientId, scopes)
+  window.chrome.identity.launchWebAuthFlow({
+    url: req,
+    interactive: true
+  }, (res) => { if (res) alert(res) })
 }
