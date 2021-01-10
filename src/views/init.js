ccc8c8bfcbd40f66fcab7005d68298557732661d
sarahhjchung <60327675+sarahhjchung@users.noreply.github.com>
2021-01-09 22:29:11 -0800

    formatted init state and started on done state

--- src/views/init.js
+++ src/views/init.js
@@ -5,9 +5,11 @@ export default (state, actions) =>
   m('main', [
     m('h1', { class: 'title' }, 'LifeOS'),
     m('h3', { class: 'message' }, 'How long would you like to work?'),
-    m('span', { class: 'circles-dark' }),
-    m('span', { class: 'circles-medium' }),
-    m('span', { class: 'circles-light' }),
+    m('div', { class: 'circles' }, [
+      m('span', { class: 'circles-dark' }),
+      m('span', { class: 'circles-medium' }),
+      m('span', { class: 'circles-light' })
+    ]), 
     m('input', { class: 'input -hours' }),
     m('input', { class: 'input -minutes' }),
     m('input', { class: 'input -seconds' }),
