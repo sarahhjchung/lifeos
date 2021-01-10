3a5ed6fe8894d635551f12b9b89d1fd75e41799b
Ning Ding <n2dingn2ding@gmail.com>
2021-01-09 20:58:51 -0800

    first binaural beats

--- src/beats.js
+++ src/beats.js
@@ -1,36 +1,29 @@
 // create web audio api context
 const context = new window.AudioContext()
 
-// const splitter = context.createChannelSplitter(2)
+const splitter = context.createChannelSplitter(2)
 const merger = context.createChannelMerger(2)
 
 
-
-// create Oscillator node
 const oscillator = context.createOscillator()
 oscillator.type = 'square'
 oscillator.frequency.setValueAtTime(440, context.currentTime)
 
-
+oscillator.connect(splitter)
+splitter.connect(merger, 0, 0)
 
 const oscillator2 = context.createOscillator();
 oscillator2.type = 'square';
-oscillator2.frequency.setValueAtTime(100, context.currentTime)
+oscillator2.frequency.setValueAtTime(430, context.currentTime)
 
-oscillator.connect(merger, 0, 0)
+oscillator2.connect(splitter)
 oscillator2.connect(merger, 0, 1)
 
 
 export default function playBeats () {
-
-    // oscillator.connect(context.destination)
-    // oscillator.start()
-
-    // oscillator2.connect(context.destination)
-    // oscillator2.start()
-
     merger.connect(context.destination)
-    merger.start()
+    oscillator.start()
+    oscillator2.start()
 
 }
 
