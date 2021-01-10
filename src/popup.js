9f4cc435d7338333f8ea29bc0054bd1ef05f21a2
sarahhjchung <60327675+sarahhjchung@users.noreply.github.com>
2021-01-09 21:14:14 -0800

    added elements to initial state

--- src/popup.js
+++ src/popup.js
@@ -39,12 +39,18 @@ const pages = {
     return m('main', [
       m('h1', { class: 'title' }, 'LifeOS'),
       m('h3', { class: 'message' }, 'How long would you like to work?'),
+      m('span', { class: 'circles-dark' }),
+      m('span', { class: 'circles-medium' }),
+      m('span', { class: 'circles-light' }),
+      m('input', { class: 'timer', id: 'inputHour' }),
+      m('input', { class: 'timer', id: 'inputMin' }),
+      m('input', { class: 'timer', id: 'inputSec' }),
       m('button', { class: 'start button', onclick: () => start() }, 'Start >')
     ])
   },
   playing () {
     return m('main', [
-      m('h1', 'Working...'),
+      m('h1', { class: 'title' }, 'Working...'),
       m('button', { onclick: () => toggle() },
         playing ? 'Pause' : 'Play'),
       m('button', { onclick: () => openSpotify() }, 'Log in with Spotify'),
